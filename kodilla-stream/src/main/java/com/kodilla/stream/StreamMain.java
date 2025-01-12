58e220b191257f454d67b1817c557279e3e926de
BasiaBmmH <barbaniella@gmail.com>
2025-01-13 00:43:33 +0100

    7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,12 +3,16 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -72,37 +76,36 @@ public class StreamMain {
 //                .forEach(System.out::println);
 
 
-        BookDirectory theBookDirectory = new BookDirectory();
-        theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .forEach(System.out::println);
-
-        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()   // [1]
-                .filter(book -> book.getYearOfPublication() > 2005)                  // [2]
-                .collect(Collectors.toList());                                       // [3]
-
-        System.out.println("# elements: " + theResultListOfBooks.size());       // [4]
-        theResultListOfBooks.stream()                                           // [5]
-                .forEach(System.out::println);
-
-
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));             // [1]
-
-        System.out.println("# elements: " + theResultMapOfBooks.size());             // [2]
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())                   // [3]
-                .forEach(System.out::println);
-
-
-        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n", "<<", ">>"));                    // [2]
-
-        System.out.println(theResultStringOfBooks);
-
+//        BookDirectory theBookDirectory = new BookDirectory();
+//        theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .forEach(System.out::println);
+//
+//        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()   // [1]
+//                .filter(book -> book.getYearOfPublication() > 2005)                  // [2]
+//                .collect(Collectors.toList());                                       // [3]
+//
+//        System.out.println("# elements: " + theResultListOfBooks.size());       // [4]
+//        theResultListOfBooks.stream()                                           // [5]
+//                .forEach(System.out::println);
+//
+//
+//        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .collect(Collectors.toMap(Book::getSignature, book -> book));             // [1]
+//
+//        System.out.println("# elements: " + theResultMapOfBooks.size());             // [2]
+//        theResultMapOfBooks.entrySet().stream()
+//                .map(entry -> entry.getKey() + ": " + entry.getValue())                   // [3]
+//                .forEach(System.out::println);
+//
+//
+//        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .map(Book::toString)
+//                .collect(Collectors.joining(",\n", "<<", ">>"));                    // [2]
+//
+//        System.out.println(theResultStringOfBooks);
 
 
 
