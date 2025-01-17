60e681b802c71bca063360f49aa90cabaa2ee33e
BasiaBmmH <barbaniella@gmail.com>
2025-01-17 15:37:15 +0100

    8.2 Zadanie: try! catch! finally!

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,55 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException");
+        } finally {
+            System.out.println("Zawsze zostanie wywołane");
+        }
+
+        System.out.println(power(2, 4));
+
+    }
+
+    public static int power(int podstawa, int wykladnik) {
+        int wynik = 1;
+
+        if (podstawa == 0) {
+            wynik = 0;
+
+        } else if (wykladnik == 0) {
+            wynik = 1;
+
+        }
+        if (wykladnik == 1) {
+            wynik = podstawa;
+
+        } else {
+            while (wykladnik > 0) {
+                wynik *= podstawa;
+                wykladnik--;
+            }
+        }
+        return wynik;
+    }
+}
