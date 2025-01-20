3a1ecf9ab81b588d5c7917515482889e0a489db6
BasiaBmmH <barbaniella@gmail.com>
2025-01-20 14:40:34 +0100

    8.4 Zadanie: wyjątki i wyszukiwarka lotów

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.test;
+
+public class Flight {
+
+    public Flight(String departureAirport, String arrivalAirport) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    String departureAirport;
+    String arrivalAirport;
+
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+}
