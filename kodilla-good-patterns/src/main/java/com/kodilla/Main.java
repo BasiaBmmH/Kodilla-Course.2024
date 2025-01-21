a3f4aa4d4acbcaa51592b8df3aa690fb6ccf0b7e
BasiaBmmH <barbaniella@gmail.com>
2025-01-21 18:23:47 +0100

    non kodilla stuff

--- kodilla-good-patterns/src/main/java/com/kodilla/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/Main.java
@@ -2,6 +2,26 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double ALGpoints = 12 + 7 + 10;
+        double ALGmax = 4 * 25;
+
+        double SKJpoints = 200 + 150 + 160 + 160 + 40 + 650  ;
+        double SKJmax = 200 + 200 + 400 + 200 + 1000 + 200 + 200 + 300 + 500 + 400 + 400;
+//        points - x
+//        max - 100
+
+
+        System.out.println(ALGpoints*100/ALGmax);
+        System.out.println(ALGmax/2);
+        System.out.println(ALGpoints);
+        System.out.println("ASD brakuje: " + (ALGmax/2 - ALGpoints));
+
+        System.out.println();
+
+        System.out.println(SKJpoints*100/SKJmax);
+        System.out.println(SKJmax/2);
+        System.out.println(SKJpoints);
+        System.out.println("SKJ brakuje: " + (SKJmax/2 - SKJpoints));
     }
 }
