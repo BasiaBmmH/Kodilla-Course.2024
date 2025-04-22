257c098a56389dce8c2a2480cd1eef207a03ded5
BasiaBmmH <barbaniella@gmail.com>
2025-04-22 19:42:10 +0200

    13.2 module

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibennateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibennateApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kodilla.kodillahibernate;
+package com.kodilla.hibernate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaHibernateApplicationTests {
+class KodillaHibennateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
